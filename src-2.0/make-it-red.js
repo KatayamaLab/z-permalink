8585000a80b18f51761bdc2e2c4b9b18ae0add8f
Noboru Katayama <katayama@rs.tus.ac.jp>
2025-07-20 11:23:49 +0900

    remove groupName

--- src-2.0/make-it-red.js
+++ src-2.0/make-it-red.js
@@ -4,7 +4,7 @@ ZoteroPermalink = {
 	rootURI: null,
 	initialized: false,
 	addedElementIDs: [],
-	
+
 	init({ id, version, rootURI }) {
 		if (this.initialized) return;
 		this.id = id;
@@ -12,17 +12,17 @@ ZoteroPermalink = {
 		this.rootURI = rootURI;
 		this.initialized = true;
 	},
-	
+
 	log(msg) {
 		Zotero.debug("Zotero Permalink: " + msg);
 	},
-	
+
 	addToWindow(window) {
 		let doc = window.document;
-		
+
 		// Use Fluent for localization
 		window.MozXULElement.insertFTLIfNeeded("make-it-red.ftl");
-		
+
 		// Add context menu item for items
 		let menuitem = doc.createXULElement('menuitem');
 		menuitem.id = 'zotero-permalink-copy-web-link';
@@ -30,7 +30,7 @@ ZoteroPermalink = {
 		menuitem.addEventListener('command', () => {
 			ZoteroPermalink.copyWebLink(window);
 		});
-		
+
 		// Add to item context menu
 		let itemMenu = doc.getElementById('zotero-itemmenu');
 		if (itemMenu) {
@@ -38,7 +38,7 @@ ZoteroPermalink = {
 			this.storeAddedElement(menuitem);
 		}
 	},
-	
+
 	addToAllWindows() {
 		var windows = Zotero.getMainWindows();
 		for (let win of windows) {
@@ -46,14 +46,14 @@ ZoteroPermalink = {
 			this.addToWindow(win);
 		}
 	},
-	
+
 	storeAddedElement(elem) {
 		if (!elem.id) {
 			throw new Error("Element must have an id");
 		}
 		this.addedElementIDs.push(elem.id);
 	},
-	
+
 	removeFromWindow(window) {
 		var doc = window.document;
 		// Remove all elements added to DOM
@@ -62,7 +62,7 @@ ZoteroPermalink = {
 		}
 		doc.querySelector('[href="make-it-red.ftl"]')?.remove();
 	},
-	
+
 	removeFromAllWindows() {
 		var windows = Zotero.getMainWindows();
 		for (let win of windows) {
@@ -70,7 +70,7 @@ ZoteroPermalink = {
 			this.removeFromWindow(win);
 		}
 	},
-	
+
 	async copyWebLink(window) {
 		try {
 			const zoteroPane = window.ZoteroPane || Zotero.getActiveZoteroPane();
@@ -78,30 +78,29 @@ ZoteroPermalink = {
 				this.log('Zotero pane not available');
 				return;
 			}
-			
+
 			const selectedItems = zoteroPane.getSelectedItems();
 			if (selectedItems.length === 0) {
 				this.log('No items selected');
 				return;
 			}
-			
+
 			const item = selectedItems[0];
 			if (!item.isRegularItem()) {
 				this.log('Selected item is not a regular item');
 				return;
 			}
-			
+
 			const itemKey = item.key;
 			const libraryID = item.libraryID;
-			
-			let groupID, groupName;
-			
+
+			let groupID;
+
 			// Check if this is a group library
 			if (libraryID !== Zotero.Libraries.userLibraryID) {
 				const group = Zotero.Groups.getByLibraryID(libraryID);
 				if (group) {
 					groupID = group.id;
-					groupName = group.name;
 				} else {
 					this.log('Could not find group for library ID: ' + libraryID);
 					return;
@@ -110,28 +109,28 @@ ZoteroPermalink = {
 				this.log('Item is in personal library, cannot generate group link');
 				return;
 			}
-			
+
 			// Generate the permalink URL
-			const permalinkURL = `https://www.zotero.org/groups/${groupID}/${groupName}/items/${itemKey}/`;
-			
+			const permalinkURL = `https://www.zotero.org/groups/${groupID}/items/${itemKey}/`;
+
 			// Copy to clipboard
 			const clipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
 				.getService(Components.interfaces.nsIClipboardHelper);
 			clipboardHelper.copyString(permalinkURL);
-			
+
 			this.log(`Copied to clipboard: ${permalinkURL}`);
-			
+
 			// Show notification
 			if (window.Zotero_Tabs && window.Zotero_Tabs.displayAlert) {
 				window.Zotero_Tabs.displayAlert('Copied web link to clipboard', 'success');
 			}
-			
+
 		} catch (error) {
 			this.log('Error copying web link: ' + error.message);
 			console.error('Zotero Permalink error:', error);
 		}
 	},
-	
+
 	async main() {
 		this.log('Zotero Permalink plugin initialized');
 	},
